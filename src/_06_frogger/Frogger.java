0b3365a5426d346f1de1c9f506d9dcaab6ab6d8a
root <root@54a09acd6975>
2022-10-11 01:26:50 +0000

    more of frogger

--- src/_06_frogger/Frogger.java
+++ src/_06_frogger/Frogger.java
@@ -6,12 +6,13 @@ public class Frogger extends PApplet {
 	static final int WIDTH = 800;
 	static final int HEIGHT = 600;
 	Frog frog = new Frog(400, 500);
-
+    Car car1 = new Car(600,700);
 	@Override
 	public void settings() {
 		size(WIDTH, HEIGHT);
-	
+
 	}
+
 	@Override
 	public void setup() {
 
@@ -19,19 +20,18 @@ public class Frogger extends PApplet {
 
 	public void keyPressed() {
 
-		if (key == CODED)
-		{
-			
-			if (keyCode == UP && frog.y>30)  {
-				frog.y-=10;
-			} else if (keyCode == DOWN && frog.y<570) {
-				frog.y+=10;
+		if (key == CODED) {
+
+			if (keyCode == UP && frog.y > 30) {
+				frog.y -= 10;
+			} else if (keyCode == DOWN && frog.y < 570) {
+				frog.y += 10;
 				// Frog Y position goes down
-			} else if (keyCode == RIGHT && frog.x<770) {
-				frog.x+=10;
+			} else if (keyCode == RIGHT && frog.x < 770) {
+				frog.x += 10;
 				// Frog X position goes right
-			} else if (keyCode == LEFT && frog.x>30) {
-				frog.x-=10;
+			} else if (keyCode == LEFT && frog.x > 30) {
+				frog.x -= 10;
 				// Frog X position goes left
 			}
 		}
@@ -41,8 +41,7 @@ public class Frogger extends PApplet {
 	public void draw() {
 
 		background(0, 0, 255);
-frog.draw();
-	
+		frog.draw();
 
 	}
 
@@ -54,17 +53,19 @@ frog.draw();
 	class Car {
 		int x = 0;
 		int y = 0;
-		
+
 		Car(int x, int y) {
 			this.x = x;
 			this.y = y;
 		}
-			void draw() {
-				fill(155,0,0);
-				rect(x,y,50,70);
-			
-			}
+
+		void draw() {
+			fill(155, 0, 0);
+			rect(x, y, 50, 70);
+
+		}
 	}
+
 	class Frog {
 		int x = 0;
 		int y = 0;
@@ -76,7 +77,7 @@ frog.draw();
 
 		void draw() {
 			fill(0, 255, 0);
-			ellipse(x,y, 50, 50);
+			ellipse(x, y, 50, 50);
 		}
 	}
 
