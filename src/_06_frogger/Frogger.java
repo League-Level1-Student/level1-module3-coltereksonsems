d3203ccd57065da0b25ae85b66d738b588216b98
root <root@e13022aad2ca>
2023-01-03 02:13:06 +0000

    finished frogger

--- src/_06_frogger/Frogger.java
+++ src/_06_frogger/Frogger.java
@@ -3,17 +3,20 @@ package _06_frogger;
 import java.util.Random;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Frogger extends PApplet {
 	static final int WIDTH = 800;
 	static final int HEIGHT = 600;
 	Random ran = new Random();
 	Frog frog = new Frog(400, 500);
-	Car car1 = new Car(ran.nextInt(400 - 1) + 1, 100);
-	Car car2 = new Car(ran.nextInt(400 - 1) + 1, 200);
+	Car car1 = new Car(ran.nextInt(400 - 1) + 1, 170);
+	Car car2 = new Car(ran.nextInt(400 - 1) + 1, 240);
 	Car car3 = new Car(ran.nextInt(400 - 1) + 1, 300);
 	Car car4 = new Car(ran.nextInt(400 - 1) + 1, 400);
-
+	PImage frogImage;
+	PImage carImage;
+	PImage backgroundImage;
 	@Override
 	public void settings() {
 		size(WIDTH, HEIGHT);
@@ -22,7 +25,12 @@ public class Frogger extends PApplet {
 
 	@Override
 	public void setup() {
-
+		frogImage = loadImage("frog.png");
+		frogImage.resize(50, 50);
+		carImage = loadImage("carRight.png");
+		carImage.resize(100, 50);
+		backgroundImage = loadImage("froggerBackground.png");
+		backgroundImage.resize(800, 600);
 	}
 
 	public void keyPressed() {
@@ -47,7 +55,7 @@ public class Frogger extends PApplet {
 	@Override
 	public void draw() {
 
-		background(0, 0, 255);
+		image (backgroundImage, 0,0);
 		frog.draw();
 		car1.draw();
 		car2.draw();
@@ -57,37 +65,53 @@ public class Frogger extends PApplet {
 		car2.moveRight();
 		car3.moveLeft();
 		car4.moveRight();
-if( intersects(car1)) {frog.resetFrog();}
-if( intersects(car2)) {frog.resetFrog();}
-if( intersects(car3)) {frog.resetFrog();}
-if( intersects(car4)) {frog.resetFrog();}
-	
+		if (intersects(car1)) {
+			frog.resetFrog();
+		}
+		if (intersects(car2)) {
+			frog.resetFrog();
+		}
+		if (intersects(car3)) {
+			frog.resetFrog();
+		}
+		if (intersects(car4)) {
+			frog.resetFrog();
+		}
+
 	}
-	
-	
-	
 
 	static public void main(String[] args) {
 		PApplet.main(Frogger.class.getName());
 	}
 
 	boolean intersects(Car car) {
-		 if ((frog.gety() > car.getY() && frog.gety() < car.getY()+50) &&
-		                (frog.getX() > car.getX() && frog.getX() < car.getX()+car.getwidth())) {
-		   return true;
-		  }
-		 else  {
-		  return false;
-		 }
+		if ((frog.gety() > car.getY() && frog.gety() < car.getY() + 50)
+				&& (frog.getX() > car.getX() && frog.getX() < car.getX() + car.getwidth())) {
+			return true;
+		} else {
+			return false;
+		}
 	}
+
 	// Make Car class here
 	class Car {
-		
-		int getX() {return x;}
-		int getY() {return y;}
-		int getwidth() {return width;}
-		int getheight() {return height;}
-		
+
+		int getX() {
+			return x;
+		}
+
+		int getY() {
+			return y;
+		}
+
+		int getwidth() {
+			return width;
+		}
+
+		int getheight() {
+			return height;
+		}
+
 		int x = 0;
 		int y = 0;
 
@@ -95,46 +119,55 @@ if( intersects(car4)) {frog.resetFrog();}
 			this.x = x;
 			this.y = y;
 		}
-int width = 100;
-int height = 50;
-		void draw() {
-			    fill(0,255,0);
-			    rect(x , y,  width, height);
-		}
-void moveLeft(){
-	x-=1;
-	if (x==-10) {
-		x=800;
-	}
-			
-}
-void moveRight() {
-	x+=1;
-	if  (x==800) {
-		x=-10;
-	}
-}
 
+		int width = 100;
+		int height = 50;
+
+		void draw() {
+			image (carImage,x,y);
+		}
+
+		void moveLeft() {
+			x -= 1;
+			if (x == -10) {
+				x = 800;
+			}
+
+		}
+
+		void moveRight() {
+			x += 1;
+			if (x == 800) {
+				x = -10;
+			}
+		}
 
 	}
 
 	class Frog {
-void resetFrog() {
-	x = 400;
-	y = 550;
-}
+		void resetFrog() {
+			x = 400;
+			y = 550;
+		}
+
 		int x = 0;
 		int y = 0;
-int getX(){return x;}
-int gety(){return y;}
+
+		int getX() {
+			return x;
+		}
+
+		int gety() {
+			return y;
+		}
+
 		Frog(int x, int y) {
 			this.x = x;
 			this.y = y;
 		}
 
 		void draw() {
-			fill(0, 255, 0);
-			ellipse(x, y, 50, 50);
+			image(frogImage, x, y);
 		}
 	}
 
